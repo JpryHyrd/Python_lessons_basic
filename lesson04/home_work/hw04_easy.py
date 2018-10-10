7ace0bcf9f68955c5ff8371e50869660582606ce
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2018-10-11 00:03:40 +0300

    Easy

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -5,14 +5,45 @@
 # Получить новый список, элементы которого будут
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
-
-# Задание-2:
+import random
+list1 = []
+list2 = []
+for i in range(random.randint(50, 100)):
+	list1.append(random.randint(1, 100))
+for i in list1:
+	list2.append(i ** 2)
+# Задание-2:import random
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
-
+import random
+fruits = ["абрикос", "черешня", "вишня", "банан", "слива", "яблоко"]
+list1 = []
+list2 = []
+list3 = []
+for i in range(5):
+	a = random.randint(0, len(fruits) -1)
+	b = random.randint(0, len(fruits) -1)
+	list1.append(fruits[a])
+	list2.append(fruits[b])
+for i in list1:
+	if i in list2:
+		list3.append(i)
+print(list1)
+print(list2)
+print(list3)
 # Задание-3:
 # Дан список, заполненный произвольными числами.
 # Получить список из элементов исходного, удовлетворяющих следующим условиям:
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+import random
+list1 = []
+list2 = []
+for i in range(random.randint(50, 100)):
+	list1.append(random.randint(1, 100))
+for i in list1:
+	if i % 3 == 0 and i > 0 and i % 4 != 0:
+		list2.append(i)
+print(list1)
+print(list2)
