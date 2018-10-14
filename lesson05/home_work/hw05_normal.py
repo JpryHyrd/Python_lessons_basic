89539ec83e72d7061c2c8c288c530855eb424f35
JpryHyrd <42839731+JpryHyrd@users.noreply.github.com>
2018-10-14 14:54:51 +0300

    Normal

--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,60 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+
+
+#Вот код файла module.py:
+def cr(x):
+	import os
+	dir_path = os.path.join(os.getcwd(), x)
+	try:
+		os.mkdir(dir_path)
+	except Exception:
+		print("\aФайл уже сущуствует!")
+	print("Успешно создано!")
+def dl(x):
+	import os
+	dir_path = os.path.join(os.getcwd(), x)
+	try:
+		os.rmdir(dir_path)
+	except Exception:
+		print("\aФайл не сущуствует!")
+	print("Успешно удалено!")
+def cod():
+	import os
+	return os.listdir()
+def soder(x):
+	import os
+	try:
+		return os.listdir(x)
+	except Exception:
+		print("\aНеверный путь!")
+
+#Теперь сам код:
+import module
+answer = None
+while True:
+	print("Выберите действие:")
+	print("\t\t1 - Просмотреть содержимое папки")
+	print("\t\t2 - Просмотреть содержимое текущей папки")
+	print("\t\t3 - Удалить папку")
+	print("\t\t4 - Создать папку")
+	print("\t\t5 - Выход")
+	answer = input("\t")
+	if answer == "5":
+		print("\n\t\t\tДо свидания!")
+		break
+	elif answer == "1":
+		name = input("Введите полный путь к папке для ее просмотра:\n\t")
+		module.soder(name)
+	elif answer == "2":
+		print("Вот содержимое текущей папки:")
+		print(module.cod())
+	elif answer == "3":
+		name = input("Введите название папки для удаления:\n\t\t")
+		module.dl(name)
+	elif answer == "4":
+		name = input("Введите название новой папки:\n\t\t")
+		module.cr(name)
+	else:
+		print("\aНеверный ввод данных! Повторите повытку снова!")
